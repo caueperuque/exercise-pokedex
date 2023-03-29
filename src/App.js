a2ff535182440531865c772ca5fcbc7f0e49dfb1
caue peruque <caue19052003@gmail.com>
2023-03-29 20:10:41 -0300

    requisito 2 finalizado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Pokedex from './components/Pokedex';
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Exercise - Pokedex</h1>
+      <Pokedex />
     );
   }
 }
---
+++ src/components/Pokedex.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Pokedex extends Component {
+  render() {
+    // const { pokemonList } = this.props;
+    return (
+      <>
+        <h1> Pokédex </h1>
+        <ul>.</ul>
+      </>
+    );
+  }
+}
